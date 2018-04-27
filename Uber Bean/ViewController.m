a370b90d355f6a22ff9df35653fff5b57e4208d3
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-04-27 14:32:21 -0400

    part 1 complete

--- Uber Bean/ViewController.m
+++ Uber Bean/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+@import MapKit;
+@import CoreLocation;
 
-@interface ViewController ()
-
+@interface ViewController () <MKMapViewDelegate,CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property CLLocationManager *locationManager;
 @end
 
 @implementation ViewController
@@ -17,6 +20,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    self.mapView.delegate = self;
+    [self.locationManager requestWhenInUseAuthorization];
+    self.mapView.showsUserLocation = YES;
 }
 
 
@@ -25,5 +33,30 @@
     [super didReceiveMemoryWarning];
 }
 
+- (void)locationManager:(CLLocationManager *)manager
+didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    NSLog(@"Authorization status: %d", status);
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse)
+    {
+        [manager requestLocation];
+        [manager startUpdatingLocation];
+    }
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations
+{
+    NSLog(@"Updated locations %@", locations);
+    CLLocation *loc = locations[0];
+    [self.mapView
+     setRegion:MKCoordinateRegionMake(loc.coordinate,
+                                      MKCoordinateSpanMake(0.06, 0.06))
+     animated:YES];
+}
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+     NSLog(@"Failed with error %@",error);
+}
 
 @end
